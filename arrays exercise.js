b9caa308c1c7561162b50e648bb4b9dd6995e94a
patricia vinga <vingapr2@gmail.com>
2019-03-04 09:50:15 +0200

    Created a list using arrays
    Created an unordered list using arrays in javascript with the items ' Harvey, has, 12 cars'.

---
+++ arrays exercise.js
@@ -0,0 +1,3 @@
+var things=[23 , "Cars ", "Harvey ", 11,];
+
+document.getElementById("demo").innerHTML="<ul>" + "<li>" + things[2] + "</li>" + "<li>" + "has" + "</li>" + "<li>" + (things[0]-things[3]) + "</li>" + "<li>" + things[1] + "</li>" + "<ul>";
