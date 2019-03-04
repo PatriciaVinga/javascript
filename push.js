c545f7af2b386d05d2b420b799a9685443ea7a0f
patricia vinga <vingapr2@gmail.com>
2019-03-04 11:09:55 +0200

    Used push a to add an element to the array
    Used push to add the element "twelve" to our array

---
+++ push.js
@@ -0,0 +1,4 @@
+var things=[23 , "Cars ", "Harvey ", 11,];
+things.push("twelve ");
+
+document.getElementById("demo").innerHTML="<ul>" + "<li>" + things[2] + "</li>" + "<li>" + "has" + "</li>" + "<li>" + things[4] + "(" + (things[0]-things[3]) + ")" + "</li>" + "<li>" + things[1] + "</li>" + "</ul>";
