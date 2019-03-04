31c6811c16c4044e4a5ddfee25c908d04b44bfe2
patricia vinga <vingapr2@gmail.com>
2019-03-04 11:56:00 +0200

    Used shift and unshift to add elements
    Used shift and unshift to add elements at the beginning of an array.

--- push.js
+++ push.js
@@ -1,12 +1,12 @@
-var cars=["BMW" , " Benz", " Toyota",];
+var cars=[" BMW" , " Benz", " Toyota",];
 
 document.getElementById("demo").innerHTML= "There are " + cars.length + " cars..." + cars.toString();
 
-cars.push(" Land Rover ");
+cars.unshift(" Lexus");
 document.getElementById("demo01").innerHTML= "There are now " + cars.length + " cars..." + cars.toString();
 
-cars.pop();
-document.getElementById("demo02").innerHTML= "There are now " + cars.length + " cars..." + cars.toString();
+cars.shift();
+document.getElementById("demo02").innerHTML= "There are " + cars.length + " cars..." + cars.toString();
 
-cars.pop();
-document.getElementById("demo02").innerHTML= "There are now " + cars.length + " cars..." + cars.toString();
+cars.shift();
+document.getElementById("demo02").innerHTML= "There are " + cars.length + " cars..." + cars.toString();
