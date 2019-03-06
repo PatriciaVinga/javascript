dd24d9594d0165eebd9e9dc676cbac09490afeed
patricia vinga <vingapr2@gmail.com>
2019-03-06 11:54:25 +0200

    Removed an element from a looping array using while
    Used the pop function to remove an element from the looping cars array using while.

--- loops exercise.js
+++ loops exercise.js
@@ -1,10 +1,11 @@
 var cars=["Benz","BMW", "Audi", "Volkswagen", "Ruf"];
-
+var i = 0;
 var text = "<ul>";
 
 cars.pop();
-for (var i = 0; i < cars.length; i++) {
+while (i < cars.length) {
     text +=  "<li>" + cars[i]  + "</li>";
+    i++;
 }
 text += "</ul>";
 
