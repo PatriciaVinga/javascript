e99676657450fc9954b1c8de400e7caaa14ac8b9
patricia vinga <vingapr2@gmail.com>
2019-03-07 09:04:07 +0200

    Created statements using secondary and tertiary ifs
    Used secondary and tertiary ifs to display statements

--- if.js
+++ if.js
@@ -1,7 +1,22 @@
 var days = ["Monday", "Tuesday", "Wednesday"];
 
+
 if (days[0] == "Monday"){
-  document.write("Hey I found Monday");
+  document.write("Hey I found Monday <br> ");
 } else {
-  document.write("Bummer man, no Monday here");
+  document.write("Bummer, no Monday");
+}
+
+
+if (days[1] == "Tuesday"){
+  document.write("Hey I found Tuesday <br>");
+} else {
+  document.write("Bummer, no Tuesday");
+}
+
+
+if (days[2] == "Wednesday"){
+  document.write("Hey I found Wednesday ");
+} else {
+  document.write("Bummer, no Wednesday <br>");
 }
