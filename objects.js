602244971110a4f8eb9da7aaf0695fab28e10932
patricia vinga <vingapr2@gmail.com>
2019-03-11 10:20:31 +0200

    Used JavaScript objects to write a statement
    Used JavaScript objects to  write the statement 'This is a Toyota car called a Corolla! It is a Sedan which is blue and was released in 1997.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var car = {
+  brand: "Toyota",
+  make: "Corolla",
+  type: "Sedan",
+  color: "blue",
+  yearOfRelease: 1997
+};
+
+document.getElementById("demo").innerHTML="This is a " + car.brand + " car called a " + car.make + "!";
+document.getElementById("demo2").innerHTML="It is a " + car.type + " and was realeased in " + car.yearOfRelease + ".";
