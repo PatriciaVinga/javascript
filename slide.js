c4a2b0bfac2d3afa4e1e78b94dbe6ac80dd850bb
patricia vinga <vingapr2@gmail.com>
2019-03-11 08:50:33 +0200

    Created a panel and gave it the slide function
    Created a panel and enabled it to slide down on clicking the 'slide down' flip and to slide up on clicking the 'panel up' button using jQuery.

---
+++ slide.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideDown("slow");
+
+  });
+});
+
+
+$(document).ready(function(){
+  $("button#panelUp").click(function(){
+    $("#panel").slideUp("slow");
+
+  });
+});
